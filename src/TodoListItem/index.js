9786204397127771a2f785a453b309817aeebee0
catli-unbounce <catherine.li@unbounce.com>
2021-09-30 11:38:46 -0700

    Got the checkbox icon working

--- src/TodoListItem/index.js
+++ src/TodoListItem/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import styles from './TodoListItem.module.scss';
+import checkIcon from '../assets/images/icon-check.svg'
 function TodoListItem({item}) {
     return ( 
             <li className={styles["list-item"]}>
-                <input type="checkbox" className={styles["list-item__checkbox"]} name="item-completed" value={item} />{item}
+                <input type="checkbox" className={styles["list-item__checkbox"]} name="item-completed" value={item} />
+                    <img className={styles["check-icon"]} src={checkIcon}></img>
+                    <span>{item}</span>
             </li> 
         );
 }
