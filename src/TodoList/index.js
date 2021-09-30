f6594561e936f87b98c26bf2a0446b88f98d6abd
catli-unbounce <catherine.li@unbounce.com>
2021-09-30 12:52:03 -0700

    adding state to react

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import TodoListItem from '../TodoListItem';
 import styles from './TodoList.module.scss';
 
-const items = [
-    'Read for 1 Hour',
-    'Pick up groceries',
-    '10 minutes of meditation'
-]
-
-const checklist = items.map((todoItem, index) => {
-    return <TodoListItem key={index} item={todoItem}></TodoListItem>
-})
 const TodoList = () => {
+
+    const [listOfItems, setListOfItems] = useState([
+        {description: 'Read for 1 Hour', completed: false},
+        {description: '10 minutes of meditation', completed: true},
+        {description: 'Go for a jog', completed: false}
+    ])
+
+    const checklist = listOfItems.map((todoItem, index) => {
+        return <TodoListItem key={index} item={todoItem.description} isCompleted={todoItem.completed}></TodoListItem>
+    })
     return (
         <ul className={styles["list"]}>
             {checklist}
--- src/TodoListItem/index.js
+++ src/TodoListItem/index.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import styles from './TodoListItem.module.scss';
 import checkIcon from '../assets/images/icon-check.svg'
-function TodoListItem({item}) {
+function TodoListItem({item, isCompleted}) {
     return ( 
             <li className={styles["list-item"]}>
-                <input type="checkbox" className={styles["list-item__checkbox"]} name="item-completed" value={item} />
+                <input type="checkbox" checked={isCompleted ? 'checked' : ''} className={styles["list-item__checkbox"]} name="item-completed" value={item} />
                     <img className={styles["check-icon"]} src={checkIcon}></img>
                     <span>{item}</span>
             </li> 
